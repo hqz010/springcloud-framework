8ffb812c6f41d9f283b9c237d072e37e1d9269f6
hqz010 <6285767@qq.com>
2018-11-02 14:05:49 +0800

    创建spring cloud通用项目框架

---
+++ springcloud-consumer/src/main/java/com/hqz/springcloudconsumer/SpringcloudConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.hqz.springcloudconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConsumerApplication.class, args);
+	}
+}
---
+++ springcloud-provider/src/main/java/com/hqz/springcloudprovider/SpringcloudProviderApplication.java
@@ -0,0 +1,12 @@
+package com.hqz.springcloudprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudProviderApplication.class, args);
+	}
+}
